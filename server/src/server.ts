8c1290d5743ae498708db614e8703282ab98facf
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2020-06-03 00:11:46 -0300

    criação do back-end da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,14 @@
-import express from 'express';
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import routes from './routes'
 
-const app = express();
+const app = express()
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-  response.json([
-    'Fabiana',
-    'Débora',
-    'João Paulo'
-  ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
-app.listen(3333);
+app.listen(3333)
